879c2165d95cd2e65a58fcc5d4c5b51f4b8130b2
fersyahhh <ferdiapriansyah99@gmail.com>
2026-02-08 21:33:34 +0700

    undo all changes

--- src/components/Layout/footer.jsx
+++ src/components/Layout/footer.jsx
@@ -1,22 +1,23 @@
+import { RedirectUrlAction } from "../../hooks/redirectUrlAction";
+
 const Footer = () => {
   const year = new Date().getFullYear();
+  const { redirectToIG } = RedirectUrlAction();
 
   return (
     <footer className="bg-charcoal">
       <div className="container px-8 py-20">
         <div>
           <h1 className="font-montserrat text-3xl font-bold text-white uppercase md:text-4xl">
-            Tamindo_Project
+            Tamindo
           </h1>
           <p className="text-light/50 font-inter mt-6 mb-8 text-sm md:max-w-[50%] md:text-base">
             A story-driven interior design practice dedicated to creating spaces
             that reflect the soul of its inhabitants.
           </p>
           <a
-            href="https://www.instagram.com/tamindo_project"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="font-inter cursor-pointer text-xs font-bold tracking-[0.2rem] text-white uppercase hover:text-white/80"
+            onClick={redirectToIG}
+            className="font-inter text-xs font-bold tracking-[0.2rem] text-white uppercase hover:text-white/80"
           >
             Instagram
           </a>
